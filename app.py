9b55063619ce629df81daccf25e24a5aa2e8e300
ilhamelizade075-bit <ilhamelizade072@gmail.com>
2026-09-01 22:52:37 +0400

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def chat():
 
     try:
         response = client.models.generate_content(
-            model="gemini-1.5-flash",
+            model="gemini-2.5-flash",
             contents=user_message,
         )
         return jsonify({"response": response.text})
